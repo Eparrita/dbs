bf30ac4ab33e64088ae0ece71b2835f2707a7e8b
Erick Parra <parritaerick@yahoo.com>
2018-03-09 15:43:11 -0300

    Se comenzo hacer el header, menu y la imagen del banner

--- gulpfile.js
+++ gulpfile.js
@@ -5,15 +5,15 @@ autoprefixer = require('gulp-autoprefixer');
 
 // Compile SASS
 gulp.task('compiled', function () {
-  gulp.src('./src/assets/sass/*.sass')
+  gulp.src('./assets/sass/*.sass')
   .pipe(sass({outputStyle:'compact'}).on('error', sass.logError))
   .pipe(autoprefixer({browsers: ['last 2 versions'],cascade: false}))
   .pipe(rename({ suffix: '.min' }))
-  .pipe(gulp.dest('./src/assets/css'))
+  .pipe(gulp.dest('./assets/css'))
 });
 
 // detect changes in SASS
 
-gulp.watch('./src/assets/sass/*.sass',['compiled']);
+gulp.watch('./assets/sass/*.sass',['compiled']);
 // Run tasks
 gulp.task('default', ['compiled']);
